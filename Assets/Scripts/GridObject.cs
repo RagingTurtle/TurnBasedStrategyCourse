7b212889d8428ce559267c5939da117d947b9fba
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2022-07-18 15:49:03 -0500

    Grid Debug Object

---
+++ Assets/Scripts/GridObject.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridObject
+{
+    private GridPosition gridPosition;
+    private GridSystem gridSystem;
+    public GridObject(GridSystem gridSystem, GridPosition gridPosition)
+    {
+        this.gridSystem = gridSystem;
+        this.gridPosition = gridPosition;
+    }
+}
