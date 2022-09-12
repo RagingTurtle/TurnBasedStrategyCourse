52279ccb88d82a9efb2fb1176cb8293f41fe1f5e
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2022-09-12 17:22:53 -0500

    Unit World UI

---
+++ Assets/Scripts/UI/UnitWorldUI.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class UnitWorldUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI actionPointsText;
+    [SerializeField] private Unit unit;
+    [SerializeField] private Image healthBarImage;
+    [SerializeField] private HealthSystem healthSystem;
+    private void Start()
+    {
+        Unit.OnAnyActionPointsChanged += Unit_OnAnyActionPointsChanged;
+        healthSystem.OnDamaged += HealthSystem_OnDamaged;
+        UpdateActionsPointsText();
+        UpdateHealthBar();
+    }
+    private void UpdateActionsPointsText()
+    {
+        actionPointsText.text = unit.GetActionPoints().ToString();
+    }
+    private void Unit_OnAnyActionPointsChanged(object sender, EventArgs e)
+    {
+        UpdateActionsPointsText();
+    }
+    private void UpdateHealthBar()
+    {
+        healthBarImage.fillAmount = healthSystem.GetHealthNormalized();
+    }
+    private void HealthSystem_OnDamaged(object sender, EventArgs e)
+    {
+        UpdateHealthBar();
+    }
+}
