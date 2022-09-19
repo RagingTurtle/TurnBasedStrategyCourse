b80fd6e64f21331bf388e22a5ce2e1a6468569ee
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2022-09-19 11:29:37 -0500

    Generic Actions

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -12,17 +12,11 @@ public class Unit : MonoBehaviour
     [SerializeField] private bool isEnemy;
     private GridPosition gridPosition;
     private HealthSystem healthSystem;
-    private MoveAction moveAction;
-    private SpinAction spinAction;
-    private ShootAction shootAction;
     private BaseAction[] baseActionArray;
     private int actionPoints = ACTION_POINTS_MAX;
     private void Awake()
     {
         healthSystem = GetComponent<HealthSystem>();
-        moveAction = GetComponent<MoveAction>();
-        spinAction = GetComponent<SpinAction>();
-        shootAction = GetComponent<ShootAction>();
         baseActionArray = GetComponents<BaseAction>();
     }
     private void Start()
@@ -43,17 +37,16 @@ public class Unit : MonoBehaviour
             LevelGrid.Instance.UnitMovedGridPosition(this, oldGridPosition, newGridPosition);
         }
     }
-    public MoveAction GetMoveAction()
+    public T GetAction<T>() where T : BaseAction
     {
-        return moveAction;
-    }
-    public SpinAction GetSpinAction()
-    {
-        return spinAction;
-    }
-    public ShootAction GetShootAction()
-    {
-        return shootAction;
+        foreach (BaseAction baseAction in baseActionArray)
+        {
+            if (baseAction is T)
+            {
+                return (T)baseAction;
+            }
+        }
+        return null;
     }
     public GridPosition GetGridPosition()
     {
