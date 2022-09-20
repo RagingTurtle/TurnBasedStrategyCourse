7afd1022d127094601e4b1d9dcfeb5ab80da567e
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2022-09-19 20:14:15 -0500

    Pathfinding Script

--- Assets/Scripts/LevelGrid.cs
+++ Assets/Scripts/LevelGrid.cs
@@ -20,7 +20,7 @@ public class LevelGrid : MonoBehaviour
         }
         Instance = this;
         gridSystem = new GridSystem<GridObject>(10, 10, 2f, (GridSystem<GridObject> g, GridPosition gridPosition) => new GridObject(g, gridPosition));
-        gridSystem.CreateDebugObjects(gridDebugObjectPrefab);
+        //gridSystem.CreateDebugObjects(gridDebugObjectPrefab);
     }
     public void AddUnitatGridPosition(GridPosition gridPosition, Unit unit)
     {
