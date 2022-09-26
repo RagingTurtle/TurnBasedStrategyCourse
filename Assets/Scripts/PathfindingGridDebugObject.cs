8fbd8a690cffd92e1cbd8a71bbb55e9eabb8a81b
RagingTurtle <93940058+RagingTurtle@users.noreply.github.com>
2022-09-26 09:33:53 -0500

    Level Design

--- Assets/Scripts/PathfindingGridDebugObject.cs
+++ Assets/Scripts/PathfindingGridDebugObject.cs
@@ -8,6 +8,7 @@ public class PathfindingGridDebugObject : GridDebugObject
     [SerializeField] private TextMeshPro gCostText;
     [SerializeField] private TextMeshPro hCostText;
     [SerializeField] private TextMeshPro fCostText;
+    [SerializeField] private SpriteRenderer isWalkableSpritRenderer;
     private PathNode pathNode;
     public override void SetGridObject(object gridObject)
     {
@@ -20,5 +21,6 @@ public class PathfindingGridDebugObject : GridDebugObject
         gCostText.text = pathNode.GetGCost().ToString();
         hCostText.text = pathNode.GetHCost().ToString();
         fCostText.text = pathNode.GetFCost().ToString();
+        isWalkableSpritRenderer.color = pathNode.IsWalkable() ? Color.green : Color.red;
     }
 }
